e20518153f7e94b9a5b5d3f88d00fc638f4dd302
Adithya-adi-Menon <adithyamenons17@gmail.com>
2022-01-30 06:43:01 -0500

    port scanner done

--- sockets.py
+++ sockets.py
@@ -1,2 +1,13 @@
 #Sockets
 
+import socket
+
+HOST = '127.0.0.1'
+PORT = 7777
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_INET - IPV4, SOCK_STREAM - PORT
+
+s.connect((HOST,PORT))
+
+# nc -nvlp 7777     
